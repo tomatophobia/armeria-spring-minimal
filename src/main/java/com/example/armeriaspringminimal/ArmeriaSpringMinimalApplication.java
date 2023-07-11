5746cda9e65cd817bd88eda6dd6b9ea6ba49581c
Youngseo Choi <ys550499@gmail.com>
2023-07-11 17:41:39 +0900

    First commit

---
+++ src/main/java/com/example/armeriaspringminimal/ArmeriaSpringMinimalApplication.java
@@ -0,0 +1,13 @@
+package com.example.armeriaspringminimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmeriaSpringMinimalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArmeriaSpringMinimalApplication.class, args);
+    }
+
+}
